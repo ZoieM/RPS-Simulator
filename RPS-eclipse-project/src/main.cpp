20ba833e647ea5f4053d22b4aae4e892059eb1e1
ZoieM <zoiemacdougall@gmail.com>
2019-05-07 12:12:18 -0700

    Created a new main.cpp, just in case.

---
+++ RPS-eclipse-project/src/main.cpp
@@ -0,0 +1,16 @@
+/*
+ * main.cpp
+ *
+ *  Created on: May 7, 2019
+ *      Author: zoie
+ */
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << "Hello World!" << endl;
+	return 0;
+}
+
